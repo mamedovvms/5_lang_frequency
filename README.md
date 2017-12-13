ce7e56a7ab997378739b5b6b0c514de73b2971af
mamedovvms <mamedovvs@mail.ru>
2017-12-13 16:43:47 +0700

    created the text of the program

--- README.md
+++ README.md
@@ -1,6 +1,26 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+The program analyzes the frequency of words in the text
+
+
+#Procedures
+
+def load_data(filepath) - returns the text of the file
+
+filepath - parameter path to the file with text
+
+def get_most_frequent_words(text) - returns a list of tuples that contain words and the number of their occurrences in the text
+
+text - the text in which the frequency of words is determined
+
+
+#Example
+
+from lang_freguency import get_most_frequent_words
+
+for word, count in get_most_frequent_words('1.txt')
+    print(word, ' - ', count)
+
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,24 @@
+import re
 
 
 def load_data(filepath):
-    pass
+
+    with open(filepath, 'rt') as f:
+        return f.read().lower()
 
 
 def get_most_frequent_words(text):
-    pass
-
+    words_text = re.findall(r'\b(?!\d)\w+\b', text)
+    set_words = {word: words_text.count(word) for word in set(words_text)}
+    return sorted(set_words.items(), key=lambda item: item[1], reverse=True)
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Add path to file: ')
+    text = load_data(filepath)
+    frequent_words = get_most_frequent_words(text)
+    i = 0
+    for word, count in frequent_words:
+        i += 1
+        print(word, ' : ', count)
+        if i >= 10:
+            break
