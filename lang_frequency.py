e2e4b483387c7d5852fb53673c78d561df21faa9
mamedovvms <mamedovvs@mail.ru>
2017-12-14 15:54:21 +0700

    created a description for the task
    Changed the program using the module collections

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,4 +1,5 @@
 import re
+import collections
 
 
 def load_data(filepath):
@@ -9,17 +10,11 @@ def load_data(filepath):
 
 def get_most_frequent_words(text):
     words_text = re.findall(r'\b(?!\d)\w+\b', text)
-    set_words = {word: words_text.count(word) for word in set(words_text)}
-    return sorted(set_words.items(), key=lambda item: item[1], reverse=True)
+    return collections.Counter(words_text).most_common(5)
 
 
 if __name__ == '__main__':
     filepath = input('Add path to file: ')
     text = load_data(filepath)
-    frequent_words = get_most_frequent_words(text)
-    i = 0
-    for word, count in frequent_words:
-        i += 1
+    for word, count in get_most_frequent_words(text):
         print(word, ' : ', count)
-        if i >= 10:
-            break
